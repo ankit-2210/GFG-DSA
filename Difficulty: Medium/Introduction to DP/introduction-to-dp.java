1b0c3483abc39a278242de52deb7666bf9d599ae
Ankit Agarwal <78948475+ankit-2210@users.noreply.github.com>
2026-06-14 13:58:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Introduction to DP/introduction-to-dp.java
@@ -0,0 +1,41 @@
+// User function Template for Java
+
+class Solution{
+    private static final int mod=1000000007;
+    
+    private static long solve(int n, long[] dp){
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+            
+        if(dp[n] != -1)
+            return dp[n];
+            
+        dp[n]=(solve(n-1, dp)+solve(n-2, dp))%mod;
+        return dp[n];
+    }
+
+    static long topDown(int n) {
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+        long[] dp=new long[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]=(dp[i-1]+dp[i-2])%mod;
+        }
+        
+        return dp[n];
+        
+    }
+
+    static long bottomUp(int n) {
+        long[] dp=new long[n+1];
+        Arrays.fill(dp, -1);
+        return solve(n, dp);
+        
+    }
+}
