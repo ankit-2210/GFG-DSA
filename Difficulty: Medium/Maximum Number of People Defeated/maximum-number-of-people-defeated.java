efcee0dd5da10f9b81a3de3bf095629a25fc9515
Ankit Agarwal <78948475+ankit-2210@users.noreply.github.com>
2026-06-23 10:25:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Number of People Defeated/maximum-number-of-people-defeated.java
+++ Difficulty: Medium/Maximum Number of People Defeated/maximum-number-of-people-defeated.java
@@ -1,16 +1,31 @@
 class Solution{
     int maxPeopleDefeated(int p){
     
-    
-        int i=1;
-        int res=0;
-        while(i*i<=p){
-            p -= (i*i);
-            res++;
-            i++;
+        long res=0;
+        long low=0, high=10000;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            long sum=mid*(mid+1)*(2*mid+1)/6;
+                
+            if(sum <= p){
+                res=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
-    
-        return res;
+        
+        return (int)res;
         
     }
 };
+
+
+
+// O(nlogn)
+
+// 1^2 + 2^2 + 3^2 + .... n^2 <= p
+// (n*(n+1)*(2n+1))/6 <= p
+
+
