ece90a9b78e240b397ff98c47a7af803070e9064
Ankit Agarwal <78948475+ankit-2210@users.noreply.github.com>
2026-09-10 17:03:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with Given GCD and LCM/pairs-with-given-gcd-and-lcm.cpp
@@ -0,0 +1,28 @@
+class Solution{
+    public:
+    int pairCount(int x, int y){
+        if(y%x != 0)
+            return 0;
+        
+        int n=y/x;
+        int d=0;
+        
+        for(int p=2; p*p<=n; p++){
+            if(n%p == 0){
+                d++;
+                
+                while(n%p == 0){
+                    n /= p;
+                }
+            }
+        }
+        
+        if(n>1){
+            d++;
+        }
+        
+        return 1<<d;
+        
+        
+    }
+};
